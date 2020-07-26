eaeafeb113c97fb8fc2ac4d03e87e9bdd108cd0d
909.pub <guoyansai@qq.com>
2020-07-26 16:32:04 +0800

    初始化工程

---
+++ pages/index/add/add-default.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="content">
+		添加
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {};
+		}
+	};
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+		margin-top: 200upx;
+	}
+</style>
