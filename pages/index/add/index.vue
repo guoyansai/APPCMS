f80a0f53caf87dffa3ba095b198e2b5383798be8
909.pub <guoyansai@qq.com>
2020-07-26 16:41:49 +0800

    修复项目初始化问题

--- pages/index/add/index.vue
+++ pages/index/add/index.vue
@@ -3,15 +3,15 @@
 		<image class="logo" :class="{'active':active}" src="../../../static/logo.png"  mode="aspectFit"></image>
 		<view class="tabbar-box-wrap">
 			<view class="tabbar-box">
-				<view class="tabbar-box-item" @click="goToPage('/pages/tabbar-3-detial/tabbar-3-release/tabbar-3-release')">
+				<view class="tabbar-box-item" @click="goToPage('/pages/index/add/add-default')">
 					<image class="box-image" src="../../../static/img/release.png" mode="aspectFit"></image>
 					<text class="explain">发图文</text>
 				</view>
-				<view class="tabbar-box-item" @click="goToPage('/pages/tabbar-3-detial/tabbar-3-video/tabbar-3-video')">
+				<view class="tabbar-box-item" @click="goToPage('/pages/index/add/add-default')">
 					<image class="box-image" src="../../../static/img/video.png" mode="aspectFit"></image>
 					<text class="explain">发视频</text>
 				</view>
-				<view class="tabbar-box-item" @click="goToPage('/pages/tabbar-3-detial/tabbar-3-qa/tabbar-3-qa')">
+				<view class="tabbar-box-item" @click="goToPage('/pages/index/add/add-default')">
 					<image class="box-image" src="../../../static/img/qa.png" mode="aspectFit"></image>
 					<text class="explain">提问</text>
 				</view>
